aed8c7841b1d41399fde14410861e69baf798750
YourFavoriteKyle <20785882+YourFavoriteKyle@users.noreply.github.com>
2023-12-07 14:03:30 -0500

    Update vitest.config.ts
    Should properly perform a coverage report now.

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,20 +15,22 @@ export default defineConfig({
 		clearMocks: true,
 		globals: true,
 		env: envVars,
-		include: ['**/*/*.ts'],
-		exclude: [
-			'**/node_modules/**',
-			'**/build/**',
-			'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-			'**/.{idea,git,cache,output,temp}/**'
-		],
 		reporters: ['default'],
 		ui: true,
 		open: false,
 		coverage: {
 			enabled: true,
 			provider: 'v8',
-			reportOnFailure: true
+			reportOnFailure: true,
+			exclude: [
+				'**/node_modules/**',
+				'**/build/**',
+				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+				'**/.{idea,git,cache,output,temp}/**',
+				'**/*.d.ts',
+				'.*.cjs'
+			],
+			all: true
 		}
 	},
 	resolve: {
